cea92836936ee2146bbba341cccbf49cac6f5dff
KerenHavro <keren11222332@gmail.com>
2024-01-31 22:28:07 +0200

    IDamagable ADDED

---
+++ Assets/IDamagable.cs
@@ -0,0 +1,56 @@
+// IDamagable interface
+public interface IDamagable
+{
+    int MaxHealth { get; }
+    int CurrentHealth { get; }
+
+    void TakeDamage(int damageAmount);
+    void Heal(int healAmount);
+    void Die();
+
+    void InflictDamage(IDamagable target);
+}
+
+// Player class implementing IDamagable
+public class Player : MonoBehaviour, IDamagable
+{
+    public int maxHealth = 100;
+    private int currentHealth;
+
+    public int MaxHealth => maxHealth;
+    public int CurrentHealth => currentHealth;
+
+    void Start()
+    {
+        currentHealth = maxHealth;
+    }
+
+    public void TakeDamage(int damageAmount)
+    {
+        currentHealth -= damageAmount;
+        if (currentHealth <= 0)
+        {
+            Die();
+        }
+    }
+
+    public void Heal(int healAmount)
+    {
+        currentHealth = Mathf.Min(maxHealth, currentHealth + healAmount);
+    }
+
+    public void Die()
+    {
+        // Handle death actions, such as respawning or game over
+        Debug.Log("Player has died!");
+    }
+
+    public void InflictDamage(IDamagable target)
+    {
+        // Assuming target is also IDamagable
+        if (target != null)
+        {
+            target.TakeDamage(10); // Inflict 10 damage
+        }
+    }
+}
