fb1969cbfe2ddd6bdd2ca831450f0fd87bdf7167
KerenHavro <keren11222332@gmail.com>
2024-02-07 21:00:55 +0200

    finished inventory

--- Assets/Scripts/ItemSlot.cs
+++ Assets/Scripts/ItemSlot.cs
@@ -135,6 +135,34 @@ public class ItemSlot : MonoBehaviour, IPointerClickHandler
 
     public void OnRightClick()
     {
+        //create a ne item
+        GameObject itemToDrop = new GameObject(itemName);
+        Item newItem = itemToDrop.AddComponent <Item>();
+        itemToDrop.AddComponent<Rigidbody2D>();
+        itemToDrop.GetComponent<Rigidbody2D>().mass = 0;
+        itemToDrop.GetComponent<Rigidbody2D>().gravityScale = 0;
+        itemToDrop.GetComponent<Rigidbody2D>().angularDrag = 0;
+
+        newItem.quantity = 1;
+        newItem.itemName = itemName;
+        newItem.sprite = itemSprite;
+        newItem.itemDescription = itemDescription;
+
+        //Create and modify the SR
+        SpriteRenderer sr = itemToDrop.AddComponent<SpriteRenderer>();
+        sr.sprite = itemSprite;
+        sr.sortingOrder = 5;
+        //sr.sortingLayerName = "UI";
+
+        //Add a collider
+        itemToDrop.AddComponent<BoxCollider2D>();
+        itemToDrop.transform.position = GameObject.FindWithTag("Player").transform.position+new Vector3(0, 2f, 0);
+
+        //sub the item
+        this.quantity -= 1;
+        quantityText.text = this.quantity.ToString();
+        if (this.quantity <= 0)
+            EmptySlot();
 
     }
 
