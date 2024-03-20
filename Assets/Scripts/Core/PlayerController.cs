c2d282fadf9fe1f12729b7f774067414a888dd84
KerenHavro <keren11222332@gmail.com>
2024-03-20 16:26:25 +0200

    dmg added

--- Assets/Scripts/Core/PlayerController.cs
+++ Assets/Scripts/Core/PlayerController.cs
@@ -182,7 +182,7 @@ public class PlayerController : MonoBehaviour
 
     void InflictDamage(IDamagable target)
     {
-        target.TakeDamage(10);
+        player.InflictDamage(target);
 
 
     }
