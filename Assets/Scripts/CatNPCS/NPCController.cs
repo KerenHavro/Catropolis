08652094cb14f6b64d97fd08dc16654ef0ac22e4
KerenHavro <keren11222332@gmail.com>
2024-05-08 12:24:41 +0300

    Cleaned up dialogue

--- Assets/Scripts/CatNPCS/NPCController.cs
+++ Assets/Scripts/CatNPCS/NPCController.cs
@@ -5,9 +5,7 @@ public class NPCController : MonoBehaviour
 {
     [SerializeField] private GameObject dialogue;
     [SerializeField] private GameObject Board;
-    [SerializeField] private GameObject patrolDestination;
-    [SerializeField] private int patrolSpeed = 10;
-    public bool walkable= false;
+
     public void Awake()
     {
         Board.gameObject.SetActive(false);
@@ -25,15 +23,8 @@ public class NPCController : MonoBehaviour
         return dialogue.activeInHierarchy;
     }
 
-    public void Update()
-    {
-        if(walkable)
-        transform.position = Vector2.MoveTowards(transform.position, patrolDestination.transform.position, patrolSpeed * Time.deltaTime);
-    }
-        public void canwalk()
-        {
-        walkable = true;
-        }
+
+   
 
     
 }
